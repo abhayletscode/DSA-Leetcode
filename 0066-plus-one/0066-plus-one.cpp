b47a0dfb619a4a709dd5082d15d1095fec3e0f7e
abhayletscode <abhaymishra72501@gmail.com>
2026-06-24 23:12:08 +0530

    Time: 0 ms (100%), Space: 11.7 MB (26.85%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& nums) {
+
+        //analsise the digit
+
+    for(int i = nums.size()-1;i>=0;i--) {
+
+        if(nums[i]<9){
+            nums[i]++;
+            return nums;
+        }
+
+        nums[i]=0;
+    }
+    nums.insert(nums.begin(),1);
+    return nums;
+        
+        
+    }
+};
